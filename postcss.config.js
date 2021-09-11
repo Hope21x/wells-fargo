92c045b07c690d11269a6ffb8b4422f50ea5ff96
Hope Kumordzie <officialhopek42@gmail.com>
2021-09-11 07:13:52 +0000

    second try

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,8 @@
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-  plugins:
-    process.env.NODE_ENV === 'production'
-      ? [
-        'postcss-flexbugs-fixes',
-        [
-          'postcss-preset-env',
-          {
-            autoprefixer: {
-              flexbox: 'no-2009',
-            },
-            stage: 3,
-            features: {
-              'custom-properties': false,
-            },
-          },
-        ],
-      ]
-      : [
-        // No transformations in development
-      ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
