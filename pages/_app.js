b7ed7275da22a554fb89e3235082207e501ee3c5
Hope Kumordzie <officialhopek42@gmail.com>
2021-09-11 19:51:27 +0000

    complete

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import 'tailwindcss/tailwind.css';
 import './tailwind.css'
+import React from 'react'
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
+  const [loaded, setLoaded] = React.useState(false);
+  React.useEffect(() => {
+    if (document.readyState !== 'loading') {
+      setLoaded(true)
+    }
+
+
+  }, []);
+  return (
+    <>
+      {
+        loaded &&
+        <Component {...pageProps} />
+      }    </>
+  )
+}
 export default MyApp
